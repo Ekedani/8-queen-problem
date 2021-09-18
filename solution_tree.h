49e3e34dd3db58295285b74c8c17820723f70c10
Vladlen <ilusenkovladlen@gmail.com>
2021-09-18 18:45:41 +0300

    Implemented solutionNode class

--- solution_tree.h
+++ solution_tree.h
@@ -4,6 +4,7 @@
 using namespace std;
 
 class solutionNode {
+private:
     vector<pair<short, short>> state;
     int depth;
     solutionNode *parentNode;
@@ -11,13 +12,48 @@ class solutionNode {
     //Possible to add "action" attribute for more obvious path
 
 public:
-    solutionNode(solutionNode &parentNode) {
+    explicit solutionNode(vector<pair<short, short>> &startState) {
+        this->state = startState;
+        this->depth = 0;
+        this->parentNode = nullptr;
+    }
 
+    solutionNode(solutionNode &parentNode) {
+        this->state = parentNode.state;
+        this->depth = parentNode.depth + 1;
+        this->parentNode = &parentNode;
+    }
+
+    void generateChildrenNodes() {
+        for (auto queenNum = 0; queenNum < state.size(); queenNum++) {
+            for (auto x = 0; x < state.size(); x++) {
+                //Horizontal movement (Changing first coordinate)
+                if (x == state[queenNum].first) {
+                    //Skipping the parent state
+                    continue;
+                }
+                auto newChild = new solutionNode(*this);
+                newChild->state[queenNum].first = x;
+                this->childrenNodes.push_back(newChild);
+            }
+        }
     }
 
     int heuristicCalc() {
-
-    };
+        int pairsInConflict = 0;
+        for (auto queenNum = 0; queenNum < state.size(); ++queenNum) {
+            for (auto queenNext = queenNum + 1; queenNext < state.size(); ++queenNext) {
+                bool notInConflict = true;
+                //Checking vertical conflict
+                notInConflict &= state[queenNum].first != state[queenNext].first;
+                //Checking diagonal conflict
+                notInConflict &= (state[queenNum].first - state[queenNext].first) ==
+                                 (state[queenNum].second - state[queenNext].second);
+                pairsInConflict += notInConflict;
+            }
+        }
+        return pairsInConflict;
+    }
 };
 
 class solutionTree {
@@ -29,5 +65,7 @@ public:
 
     }
 
+    vector<pair<short, short>> findSolutionRBFS() {
 
+    }
 };
